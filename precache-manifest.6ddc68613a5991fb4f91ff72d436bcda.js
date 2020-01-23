70bb649795e3bbc7ee00755e06f8e227c1c40435
id16jhd <id16jhd@cs.umu.se>
2020-01-23 11:14:11 +0100

    Updates

---
+++ precache-manifest.6ddc68613a5991fb4f91ff72d436bcda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8924dd920c9e727a33aaffdcb04d0cf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a88736940ae097654c63",
+    "url": "/static/css/main.0abf0388.chunk.css"
+  },
+  {
+    "revision": "edfa962ad423b3e2c591",
+    "url": "/static/js/2.fdf67c21.chunk.js"
+  },
+  {
+    "revision": "a88736940ae097654c63",
+    "url": "/static/js/main.e46a90ee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "32cae961866ac8589332de71bc99b337",
+    "url": "/static/media/kazoo.32cae961.mp3"
+  }
+]);
